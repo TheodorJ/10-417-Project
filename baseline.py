29cc6d94312b1b10172a395f2637bf8dc21ecebb
Oscar Dadfar <cardadfar@gmail.com>
2019-10-20 19:10:10 -0400

    added NUM_CORES feature

--- baseline.py
+++ baseline.py
@@ -1,86 +1,121 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import multiprocessing
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
 import torch.nn as nn
 import torch.nn.functional as F
 
-in_channels = 3
-filter_size1 = 5
-filter_size2 = 6
-inter_channels = 6
-out_channels = 16
-pool_size = 2
-layer_size1 = 120
-layer_size2 = 84
 
-num_epochs = 2
+import multiprocessing
+import math
+import csv
+
+
+from dimensions import *
+
+
+
+transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+n_test  = len(testset)
+n_train = len(trainset)
+c_in    = trainset[0][0].shape[0]
+h_in    = trainset[0][0].shape[1]
+w_in    = trainset[0][0].shape[2]
+
+
+num_epochs = 1
 lr = 0.001
 momentum = 0.9
 
 filter_size1_tries = [4, 5]
-filter_size2_tries = [4, 5]
+filter_size2_tries = [4]
 inter_channels_tries = [5, 6, 7]
 out_channels_tries = [12, 16, 32]
 pool_size_tries = [2]
-layer_size1_tries = [80, 120]
-layer_size2_tries = [84]
-num_epochs_tries = [2]
+hidden_layer_tries = [[80,84], [120,84]]
 lr_tries = [0.001]
 momentum_tries = [0.9]
 
+NUM_CORES = 4
 
+
+
+
+def build_permutations():
+
+    hyperparameters = []
+    for filter_size1 in filter_size1_tries:
+        for filter_size2 in filter_size2_tries:
+            for inter_channels in inter_channels_tries:
+                for out_channels in out_channels_tries:
+                    for pool_size in pool_size_tries:
+                        for hidden_layer in hidden_layer_tries:
+                            for lr in lr_tries:
+                                for momentum in momentum_tries:
+                                    hyperparameters.append( (filter_size1, filter_size2, inter_channels, out_channels, pool_size, hidden_layer, lr, momentum) )
+
+    return hyperparameters
+
+
+hyperparameters = build_permutations()
+
+
+def train_parameters_range(worker_id):
+
+    for j in range(math.ceil(len(hyperparameters) / NUM_CORES)):
+            indx = j * NUM_CORES + worker_id
+            if indx < len(hyperparameters):
+                hyp = hyperparameters[indx]
+                train_parameters(hyp)
 
                                     
-def train_parameters(filter_size1, filter_size2, inter_channels, out_channels, pool_size, layer_size1, layer_size2, num_epochs, lr, momentum):
+def train_parameters(hyp):
+
+    filter_size1, filter_size2, inter_channels, out_channels, pool_size, hidden_layer, lr, momentum = hyp
+    
     print("\nTesting for:")
     print("  filter_size1 = %s" % filter_size1)
     print("  filter_size2 = %s" % filter_size2)
     print("  inter_channels = %s" % inter_channels)
     print("  out_channels = %s" % out_channels)
     print("  pool_size = %s" % pool_size)
-    print("  layer_size1 = %s" % layer_size1)
-    print("  layer_size2 = %s" % layer_size2)
-    print("  num_epochs = %s" % num_epochs)
+    print("  hidden_layers = %s" % hidden_layer)
     print("  lr = %s" % lr)
     print("  momentum = %s" % momentum)
+    
+    hl = hidden_layer.copy()
+    hl.insert(0, out_channels * 7 * 7)
+    hl.append(10)
 
     class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(in_channels, inter_channels, filter_size1, padding=1)
+            self.conv1 = nn.Conv2d(c_in, inter_channels, filter_size1, padding=1)
             self.pool = nn.MaxPool2d(pool_size, pool_size)
             self.conv2 = nn.Conv2d(inter_channels, out_channels, filter_size2, padding=1)
-            self.fc1 = nn.Linear(out_channels * 7 * 7, layer_size1)
-            self.fc2 = nn.Linear(layer_size1, layer_size2)
-            self.fc3 = nn.Linear(layer_size2, 10)
+
+            decoder = []
+            for i in range(len(hl)-1):
+                decoder.append( nn.Linear(hl[i], hl[i+1]) )
+                decoder.append( nn.ReLU() )
+            self.decoder = nn.Sequential( *decoder )
+            
 
         def forward(self, x):
             x = self.pool(F.relu(self.conv1(x)))
             x = self.pool(F.relu(self.conv2(x)))
             x = x.view(-1, int(x.numel()/x.shape[0]))
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = self.fc3(x)
+            x = self.decoder(x)
             return x
 
 
@@ -96,6 +131,8 @@ def train_parameters(filter_size1, filter_size2, inter_channels, out_channels, p
     for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         running_loss = 0.0
+        total_loss   = 0.0
+        
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
@@ -111,16 +148,14 @@ def train_parameters(filter_size1, filter_size2, inter_channels, out_channels, p
 
             # print statistics
             running_loss += loss.item()
+            total_loss   += loss.item()
             if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                        (epoch + 1, i + 1, running_loss / 2000))
+                print('[%d/%d, %5d/%5d] loss: %.3f' %
+                        (epoch + 1, num_epochs, i + 1, len(trainloader), running_loss / 2000))
                 running_loss = 0.0
+        
 
     print('Finished Training')
-
-
-    """PATH = './cifar_net.pth'
-    torch.save(net.state_dict(), PATH)
 
     dataiter = iter(testloader)
     images, labels = dataiter.next()
@@ -135,31 +170,17 @@ def train_parameters(filter_size1, filter_size2, inter_channels, out_channels, p
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
+    net_loss = total_loss / n_train
+    net_acc  = correct / n_test
 
-
-
-    # print images
-    imshow(torchvision.utils.make_grid(images))
-    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))"""
-
+    print('Net Loss: %.3f   | Net Acc: %.3f' % (net_loss, net_acc))
 
 
 
 
 workers = []
-for filter_size1 in filter_size1_tries:
-    for filter_size2 in filter_size2_tries:
-        for inter_channels in inter_channels_tries:
-            for out_channels in out_channels_tries:
-                for pool_size in pool_size_tries:
-                    for layer_size1 in layer_size1_tries:
-                        for layer_size2 in layer_size2_tries:
-                            for num_epochs in num_epochs_tries:
-                                for lr in lr_tries:
-                                    for momentum in momentum_tries:
-                                        workers.append(multiprocessing.Process(target=train_parameters, args=(filter_size1, filter_size2, inter_channels, out_channels, pool_size, layer_size1, layer_size2, num_epochs, lr, momentum,)))
+for i in range(NUM_CORES):
+    workers.append(multiprocessing.Process(target=train_parameters_range, args=(i,)))
 
 for worker in workers:
     worker.start()
@@ -167,31 +188,3 @@ for worker in workers:
 for worker in workers:
     worker.join()
 
-workers = []
-for filter_size1 in filter_size1_tries:
-    for filter_size2 in filter_size2_tries:
-        for inter_channels in inter_channels_tries:
-            for out_channels in out_channels_tries:
-                for pool_size in pool_size_tries:
-                    for layer_size1 in layer_size1_tries:
-                        for layer_size2 in layer_size2_tries:
-                            for num_epochs in num_epochs_tries:
-                                for lr in lr_tries:
-                                    for momentum in momentum_tries:
-                                        workers.append(multiprocessing.Process(target=train_parameters, args=(filter_size1, filter_size2, inter_channels, out_channels, pool_size, layer_size1, layer_size2, num_epochs, lr, momentum,)))
-
-for worker in workers:
-    worker.start()
-
-for worker in workers:
-    worker.join()
-
-net = Net()
-net.load_state_dict(torch.load(PATH))
-
-outputs = net(images)
-
-_, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
---
+++ dimensions.py
@@ -0,0 +1,12 @@
+def conv_dimensions(c_in, h_in, w_in, c_out, stride, pad, k_height, k_width):
+
+	c_out = c_out
+	h_out = (h_in - k_height + 2*pad)//stride + 1 
+	w_out = (w_in - k_width  + 2*pad)//stride + 1
+
+	return c_out, h_out, w_out
+
+
+
+
+
