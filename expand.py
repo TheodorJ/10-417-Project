f64b7df0290f77d76ebc1e68996b4ffc2d91a9ac
TheodorJ <teddyjohansson@live.com>
2019-11-17 19:15:27 -0500

    Implemented generate_all_modifications

--- expand.py
+++ expand.py
@@ -274,102 +274,128 @@ def descriptor_to_network(descriptor):
 
     return Net()
 
+# Given a descriptor, return every possible descriptor that can be made by making
+# a single change to this descriptor
+def generate_all_modifications(descriptor):
+
+    mutations
+
+    conv_index = 0
+    line_index = 0
+    for layer in descriptor:
+        layer_type = layer[0]
+        if layer_type == "Conv2d":
+
+            mutations += [insert_hidden_filter(descriptor, conv_index)]
+            mutations += [expand_conv_layer(descriptor, conv_index)]
+
+            conv_index += 1
+        elif layer_type = "Linear":
+            mutations += [insert_hidden_units(descriptor, line_index)]
+
+            line_index += 1
+
+    mutations += [insert_conv_layer(descriptor)]
+    mutations += [insert_layer(descriptor)]
+
+    return mutations
 
 
-c_out, h_out, w_out = conv_dimensions(3, 33, 33, 5, 1, 2, 4, 4)
-net = descriptor_to_network([("Conv2d", torch.zeros((3, 3, 4, 4)), torch.zeros((5,))), ("ReLU",), \
- ("Conv2d", torch.zeros((5, 3, 4, 4)), torch.zeros((5,))), \
- ("ReLU",), ("Flatten",),  \
- ("Linear", torch.zeros((84, c_out * h_out * w_out)), torch.zeros((84,))), ("ReLU",), \
- ("Linear", torch.zeros((10, 84)), torch.zeros((10,)))])
+if __name__=="__main__":
+    c_out, h_out, w_out = conv_dimensions(3, 33, 33, 5, 1, 2, 4, 4)
+    net = descriptor_to_network([("Conv2d", torch.zeros((3, 3, 4, 4)), torch.zeros((5,))), ("ReLU",), \
+     ("Conv2d", torch.zeros((5, 3, 4, 4)), torch.zeros((5,))), \
+     ("ReLU",), ("Flatten",),  \
+     ("Linear", torch.zeros((84, c_out * h_out * w_out)), torch.zeros((84,))), ("ReLU",), \
+     ("Linear", torch.zeros((10, 84)), torch.zeros((10,)))])
 
 
 
-transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transform = transforms.Compose(
+                [transforms.ToTensor(),
+                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-n_test  = len(testset)
-n_train = len(trainset)
-c_in    = trainset[0][0].shape[0]
-h_in    = trainset[0][0].shape[1]
-w_in    = trainset[0][0].shape[2]
+    n_test  = len(testset)
+    n_train = len(trainset)
+    c_in    = trainset[0][0].shape[0]
+    h_in    = trainset[0][0].shape[1]
+    w_in    = trainset[0][0].shape[2]
 
-import torch.optim as optim
+    import torch.optim as optim
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
 
-for epoch in range(1):  # loop over the dataset multiple times
+    for epoch in range(1):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    total_loss   = 0.0
-    correct_train = 0
+        running_loss = 0.0
+        total_loss   = 0.0
+        correct_train = 0
 
-    # Time code found here: https://stackoverflow.com/questions/5998245/get-current-time-in-milliseconds-in-python
-    starttime = int(round(time.time() * 1000))
+        # Time code found here: https://stackoverflow.com/questions/5998245/get-current-time-in-milliseconds-in-python
+        starttime = int(round(time.time() * 1000))
 
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        total_loss   += loss.item()
-
-        _, predicted = torch.max(outputs.data, 1)
-        correct_train += (predicted == labels).sum().item()
-
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d/%d, %5d/%5d] loss: %.3f' %
-                    (epoch + 1, 2, i + 1, len(trainloader), running_loss / 2000))
-            running_loss = 0.0
-
-    # Calculate test loss/accuracy
-    dataiter = iter(testloader)
-    images, labels = dataiter.next()
-
-    total = 0
-    correct = 0
-    total_test_loss = 0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            outputs = net(images)
+            # forward + backward + optimize
+            outputs = net(inputs)
             loss = criterion(outputs, labels)
-            total_test_loss += loss
-            outputs = net(images)
-            loss, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            loss.backward()
+            optimizer.step()
 
-    print(correct / total)
-    print(total_test_loss.item())
+            # print statistics
+            running_loss += loss.item()
+            total_loss   += loss.item()
 
-    total_test_loss = total_test_loss / n_test
+            _, predicted = torch.max(outputs.data, 1)
+            correct_train += (predicted == labels).sum().item()
 
-    endtime = int(round(time.time() * 1000))
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d/%d, %5d/%5d] loss: %.3f' %
+                        (epoch + 1, 2, i + 1, len(trainloader), running_loss / 2000))
+                running_loss = 0.0
 
-new_descriptor = net.to_descriptor()
+        # Calculate test loss/accuracy
+        dataiter = iter(testloader)
+        images, labels = dataiter.next()
 
-new_descriptor = insert_hidden_filter(new_descriptor, 0)
+        total = 0
+        correct = 0
+        total_test_loss = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = net(images)
+                loss = criterion(outputs, labels)
+                total_test_loss += loss
+                outputs = net(images)
+                loss, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-net = descriptor_to_network(new_descriptor)
+        print(correct / total)
+        print(total_test_loss.item())
+
+        total_test_loss = total_test_loss / n_test
+
+        endtime = int(round(time.time() * 1000))
+
+    new_descriptor = net.to_descriptor()
+
+    new_descriptor = insert_hidden_filter(new_descriptor, 0)
+
+    net = descriptor_to_network(new_descriptor)
