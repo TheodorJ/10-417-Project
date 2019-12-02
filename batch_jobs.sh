e0ae9dd1bc7de3eba7a2426b1ea2797637003943
TheodorJ <teddyjohansson@live.com>
2019-12-01 17:19:20 -0800

    batch_jobs now ready to run

--- batch_jobs.sh
+++ batch_jobs.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-- Mutation learning, 1 convolution & 1 maxpool layer
-- Mutation learning, 1 convolution & 1 max pool layer, no knowledge transfer
-- Mutation learning, 2 convolution & 1 maxpool layer
-- Mutation learning, 2 convolution & 2 maxpool layer
-
 mkdir mutation_results/
+echo "Mutation learning, 1conv_1maxp"
 timeout 2h python3 expand.py --results_file mutation_results/1conv_1maxp.csv
+echo "Mutation learning, 1conv_1maxp_nokt"
 timeout 2h python3 expand.py --no_knowledge_transfer --results_file mutation_results/1conv_1maxp_nokt.csv
+echo "Mutation learning, 2conv_1maxp"
 timeout 2h python3 expand.py --two_conv --results_file mutation_results/2conv_1maxp.csv
+echo "Mutation learning, 2conv_1maxp_nokt"
 timeout 2h python3 expand.py --two_conv --no_knowledge_transfer --results_file mutation_results/2conv_1maxp_nokt.csv
-timeout 2h python3 expand.py --two_conv_mp --results_file mutation_results/2conv_2maxp_.csv
+echo "Mutation learning, 2conv_2maxp"
+timeout 2h python3 expand.py --two_conv_mp --results_file mutation_results/2conv_2maxp.csv
+echo "Mutation learning, 2conv_2maxp_nokt"
 timeout 2h python3 expand.py --two_conv_mp --no_knowledge_transfer --results_file mutation_results/2conv_2maxp_nokt.csv
+echo "Serial Baseline"
 python3 baseline.py
