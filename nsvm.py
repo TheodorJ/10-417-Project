7ca186cc1f29017ee26d67e5fc54570b9be529b3
TheodorJ <teddyjohansson@live.com>
2019-12-03 02:06:46 -0500

    Less frequent printing

--- nsvm.py
+++ nsvm.py
@@ -151,7 +151,7 @@ for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 20 == 19:    # print every 2000 mini-batches
+        if i % 80 == 79:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
@@ -225,7 +225,7 @@ for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 20 == 19:    # print every 2000 mini-batches
+        if i % 80 == 79:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
@@ -302,7 +302,7 @@ for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 20 == 19:    # print every 2000 mini-batches
+        if i % 80 == 79:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
@@ -375,7 +375,7 @@ for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 20 == 19:    # print every 2000 mini-batches
+        if i % 80 == 79:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
@@ -449,7 +449,7 @@ for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 20 == 19:    # print every 2000 mini-batches
+        if i % 80 == 79:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
